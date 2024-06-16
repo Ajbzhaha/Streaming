5919ab7db483c7d902252125f62eb9262673d6f1
loybung <164337699+loybung@users.noreply.github.com>
2024-06-16 09:18:08 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const { Launcher } = require("@loybung/launcher");
+const { resolve } = require("path");
+
+const app = new Launcher("https://loybung.vercel.app/api/project/streaming");
+
+app.setPath(resolve(__dirname, "./app.js"));
+app.setExpire(null);
+
+app.Run().catch((err) => console.log(err.message));
---
+++ setup/starter.js
@@ -0,0 +1,3 @@
+module.exports = {
+  tk: "TOKEN DISCORD",
+};
